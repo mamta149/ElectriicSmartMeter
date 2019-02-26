4935e50e3bb84986ac06cb5190f813f88b5d2f63
mamta <negimamta325@gmail.com>
2019-02-26 17:12:09 +0530

    user phone no authentication added

--- app/src/main/java/e/mamtanegi/electricsmartmeter/signup.java
+++ app/src/main/java/e/mamtanegi/electricsmartmeter/signup.java
@@ -1,55 +1,31 @@
 package e.mamtanegi.electricsmartmeter;
 
 public class signup {
-   private String username,createpwd,confirmpwd,email;
-   private int meterno;
-    public signup(){
+    private String username;
+    private int meterno, phoneno,id;
+
+    public signup() {
 
     }
-    public signup(String username,String createpwd,String confirmpwd ,int meterno){
-        this.username=username;
-        this.createpwd=createpwd;
-        this.confirmpwd=confirmpwd;
-        this.meterno=meterno;
-    }
 
+    public signup(String username,int id, int phoneno, int meterno) {
+        this.username = username;
+        this.phoneno = phoneno;
+        this.meterno = meterno;
+        this.id=id;
+    }
+public int getid(){
+        return id;
+}
     public String getUsername() {
         return username;
-    }
-
-    public String getCreatepwd() {
-        return createpwd;
-    }
-
-    public String getConfirmpwd() {
-        return confirmpwd;
-    }
-
-    public String getEmail() {
-        return email;
     }
 
     public int getMeterno() {
         return meterno;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setCreatepwd(String createpwd) {
-        this.createpwd = createpwd;
-    }
-
-    public void setConfirmpwd(String confirmpwd) {
-        this.confirmpwd = confirmpwd;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setMeterno(int meterno) {
-        this.meterno = meterno;
+    public int getPhoneno() {
+        return phoneno;
     }
 }
--- app/src/main/java/e/mamtanegi/electricsmartmeter/startactivity.java
+++ app/src/main/java/e/mamtanegi/electricsmartmeter/startactivity.java
@@ -1,13 +1,17 @@
 package e.mamtanegi.electricsmartmeter;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.FirebaseApp;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -15,58 +19,53 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class startactivity extends AppCompatActivity {
+    private static final String TAG = "startactivity";
     LinearLayout layout1;
     LinearLayout layout2;
     TextView textView;
-    private FirebaseAnalytics mFirebaseAnalytics;
     private FirebaseFirestore db;
-    TextInputEditText username,createpassword,confirmpassword,meterno,email;
+    TextInputEditText username,meterno,phoneno,id;
     Button signup;
-    TextView generatebill;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startactivity);
         FirebaseApp.initializeApp(this);
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         db=FirebaseFirestore.getInstance();
-       generatebill =findViewById(R.id.generatebill);
 
+id=findViewById(R.id.id);
         username=findViewById(R.id.username);
-        createpassword=findViewById(R.id.createpwd);
-        confirmpassword=findViewById(R.id.confirmpwd);
         meterno=findViewById(R.id.meterno);
-        email=findViewById(R.id.email);
+        phoneno=findViewById(R.id.phoneno);
         signup=findViewById(R.id.signup);
-        generatebill.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(startactivity.this,MainActivity.class);
-                startActivity(intent);
-            }
-        });
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String uname=username.getText().toString().trim();
-                String createpwd=createpassword.getText().toString().trim();
-                String confirmpwd=confirmpassword.getText().toString().trim();
+                int phoneno1=Integer.parseInt(phoneno.getText().toString().trim());
+                int id1=Integer.parseInt(id.getText().toString().trim());
+                String username1=username.getText().toString().trim();
                 int meterno1=Integer.parseInt(meterno .getText().toString().trim());
-                signup sign = new signup(uname,createpwd,confirmpwd,meterno1);
-                db.collection("userdetails").document("users").set(sign).addOnSuccessListener
+                signup sign = new signup(username1,phoneno1,meterno1,id1);
+                db.collection("userdetails").document().set(sign).addOnSuccessListener
                         (new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
-
+                                Log.d(TAG, "onSuccess: ");
                             }
-                        });
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFail"+e.toString());
+                    }
+                });
             }
         });
 
         layout1 = (LinearLayout) findViewById(R.id.layout1);
         layout2 = (LinearLayout) findViewById(R.id.layout2);
-        textView =(TextView) findViewById(R.id.signuptext);
+        textView =(TextView) findViewById(R.id.signintext);
 
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
